5a347c75cb6a6fc2e596c738d413b256096ce08f
Karan Singh <karan.kumar@musquaretech.com>
2023-10-12 23:47:34 +0530

    aster

---
+++ Netflix/src/Pages/Home Screen/Banner.jsx
@@ -0,0 +1,28 @@
+import "./Banner.css";
+import { Grid, Typography, Button } from "@mui/material";
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import AddIcon from '@mui/icons-material/Add';
+const Banner = () => {
+    return (
+        <div className='Banner'>
+            <Grid container p={5}>
+                <Grid item>
+                    <Typography variant="h1" color={"goldenrod"} p={4}> Animal </Typography>
+                    <Grid item maxWidth={470}>
+                        <Typography variant="body" color={"black"} textAlign={"justify"} p={4}>Expectations were high from Ranbir Kapoor Animal teaser. And they were met. On the actors birthday on Thursday, the makers dropped the Animal teaser, and it promises a fierce, exciting, gory action film. Sharing the teaser, filmmaker Sandeep Reddy Vanga wrote on X (formerly Twitter), “Check it out guys</Typography>
+                    </Grid>
+                </Grid>
+                <Grid container p={4} display={"flex"} >
+                    <Grid item p={2} >
+                        <Button variant="outlined" color="error">Play<PlayArrowIcon /></Button>
+                    </Grid>
+                    <Grid item p={2} >
+                        <Button variant="outlined" color="error">My List<AddIcon /></Button>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </div>
+    )
+}
+
+export default Banner;
---
+++ Netflix/src/Pages/Home Screen/Header.jsx
@@ -0,0 +1,32 @@
+import NetflixLogo from "../../assets/SVG/NetflixLogo.png";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import SearchIcon from '@mui/icons-material/Search';
+import { Grid } from "@mui/material";
+import { Link } from 'react-router-dom';
+import "./Header.css";
+
+function Header() {
+    return (
+        <Grid container className="Header" display={"flex"} justifyContent={"space-between"} p={1}>
+            <Grid item maxWidth={15}>
+                <img className="NetflixLogo" src={NetflixLogo} alt="A Netflix Logo" />
+            </Grid>
+            <Grid item display={"flex"} justifyContent={"space-evenly"}>
+                <Link className="Link" to="/Popular">Popular</Link>
+                <Link className="Link" to="/Watching">Watching</Link>
+                <Link className="Link" to="/TopRated">Top Rated</Link>
+                <Link className="Link" to="/Fav">Favourite</Link>
+            </Grid>
+            <Grid item display={"flex"} justifyContent={"space-evenly"} className="Logo">
+                <Grid item color={"white"}>
+                    <SearchIcon className="Icon" />
+                </Grid>
+                <Grid item color={"white"} >
+                    <AccountCircleIcon className="Icon" />
+                </Grid>
+            </Grid>
+        </Grid>
+    )
+}
+
+export default Header;
